e7040dcdb441415d75d02eae905ac229272de950
progdev8201 <56039386+progdev8201@users.noreply.github.com>
2020-04-27 12:56:51 -0400

    ai
    navmesh + zombies

---
+++ ProjectTerminus/Assets/Scripts/ZombiesNav/ZombieController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieController : MonoBehaviour
+{
+    public Camera cam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            //Create a ray toward the mouse click position
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+
+            // We shoot the ray and we gather information about what we hit
+            if(Physics.Raycast(ray, out hit))
+            {
+
+            }
+
+        }
+    }
+}
