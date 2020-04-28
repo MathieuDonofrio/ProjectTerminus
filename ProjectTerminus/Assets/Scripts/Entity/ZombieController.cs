eaa2d9ed9db979aafb4b53844cea53dbc00caf36
progdev8201 <56039386+progdev8201@users.noreply.github.com>
2020-04-27 22:47:34 -0400

    basic zombies ai

--- ProjectTerminus/Assets/Scripts/Entity/ZombieController.cs
+++ ProjectTerminus/Assets/Scripts/Entity/ZombieController.cs
@@ -2,14 +2,20 @@
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Animator))]
 public class ZombieController : MonoBehaviour
 {
     public Camera cam;
     private NavMeshAgent agent;
+    private int rotationSpeed = 10;
+    private Animator basicZombieAnimator;
+    private bool walking = false;
+    private bool attacking = false;
 
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        basicZombieAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -28,8 +34,35 @@ public class ZombieController : MonoBehaviour
                 // MOVE OUR AGENT
                 agent.SetDestination(hit.point);
 
+                //Make the character rotate
+                var dirVector = hit.point - transform.position;
+                transform.rotation = Quaternion.Slerp
+                (transform.rotation, Quaternion.LookRotation(dirVector), rotationSpeed * Time.deltaTime);
             }
 
+            //executing an action depending on if our player is moving
+            if (!IsMoving())
+            {
+                //Set the animations
+                walking = true;
+                attacking = false;
+                Debug.Log("Im moving");
+            }
+            else
+            {
+                walking = false;
+                attacking = true;
+                Debug.Log("Im supposed to be attacking");
+            }
+
+            basicZombieAnimator.SetBool("walking",walking);
+            basicZombieAnimator.SetBool("attacking",attacking);
+
         }
+    }
+
+    public bool IsMoving()
+    {
+        return agent.remainingDistance <= agent.stoppingDistance;
     }
 }
