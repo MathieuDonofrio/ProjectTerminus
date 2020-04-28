b71c3fb9819c8af4b2b4a07e7a9df76bf615154f
progdev8201 <56039386+progdev8201@users.noreply.github.com>
2020-04-27 21:24:36 -0400

    Revert "zombie ai added"
    This reverts commit fedb7a42ce8c9e5fb069c3c791b3871285f0d606.

--- ProjectTerminus/Assets/Scripts/Entity/ZombieController.cs
+++ ProjectTerminus/Assets/Scripts/Entity/ZombieController.cs
@@ -1,26 +1,15 @@
 ﻿using UnityEngine;
 using UnityEngine.AI;
-using UnityStandardAssets.Characters.ThirdPerson;
 
 [RequireComponent(typeof(NavMeshAgent))]
-[RequireComponent(typeof(Animator))]
-
 public class ZombieController : MonoBehaviour
 {
     public Camera cam;
-    public int rotationSpeed = 5;
     private NavMeshAgent agent;
-    private bool walking = false;
-    private bool attacking = false;
-    private Animator zombieAnimator;
-    private RaycastHit hit;
-    
 
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        zombieAnimator = GetComponent<Animator>();
-        agent.updateRotation = false;
     }
 
     // Update is called once per frame
@@ -30,6 +19,7 @@ public class ZombieController : MonoBehaviour
         {
             //Create a ray toward the mouse click position
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
 
 
             // We shoot the ray and we gather information about what we hit
@@ -39,29 +29,7 @@ public class ZombieController : MonoBehaviour
                 agent.SetDestination(hit.point);
 
             }
+
         }
-
-        // set the animations depending on our velocity
-
-        if (agent.remainingDistance > agent.stoppingDistance)
-        {
-            //Set the animations
-            walking = true;
-            attacking = false;
-          
-            // rotate the zombie toward the target
-            var dirVector = hit.point - transform.position;
-            transform.rotation = Quaternion.Slerp
-            (transform.rotation, Quaternion.LookRotation(dirVector), rotationSpeed * Time.deltaTime);
-        }
-        else
-        {
-            attacking = true;
-            walking = false;
-        }
-
-        zombieAnimator.SetBool("attack",attacking);
-        zombieAnimator.SetBool("walking",walking);
-
     }
 }
