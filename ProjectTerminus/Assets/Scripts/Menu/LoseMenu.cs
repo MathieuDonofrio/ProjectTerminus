be8ff8fd66c1e775d7a4e9467383c8c07f42dd94
progdev8201 <56039386+progdev8201@users.noreply.github.com>
2020-05-18 00:53:21 -0400

    purchase canvas and knife

---
+++ ProjectTerminus/Assets/Scripts/Menu/LoseMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoseMenu : MonoBehaviour
+{
+    private Button menuButton;
+    public Text roundSurvivedText;
+
+    void Start()
+    {
+        menuButton = GetComponentInChildren<Button>();
+        menuButton.onClick.AddListener(MainMenu);
+    }
+
+    public void SetRoundSurvived(int roundSurvived)
+    {
+        roundSurvivedText.text = "ROUND SURVIVED: " + roundSurvived;
+    }
+   
+    void MainMenu()
+    {
+        FindObjectOfType<MySceneManager>().LoadMainMenu();
+    }
+}
